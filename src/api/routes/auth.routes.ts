b45fdee7db013b1bd3dcf94cf92f3b0ceac711af
Wolffoner <wolffoner@gmail.com>
2022-08-20 19:45:31 -0300

    routes

--- src/api/routes/auth.routes.ts
+++ src/api/routes/auth.routes.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import authService from "../services/auth.service";
 
 const router = express.Router();
 
-router.post('/authenticate', (_req, res) => {
-    res.send("authorized");
+router.post('/authenticate', async (req: Request, res: Response) => {
+    try {
+        res.send(await authService.setToken(req.body));
+    } catch (error: any) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
 export default router;
--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -1,27 +1,34 @@
-import express from 'express';
+import express, { Request, Response } from "express";
 import userService from '../services/user.service';
+import { validationRequest } from '../utils/validations/request.validation';
 
 const router = express.Router();
 
-router.get('/users/me', async (_req, res) => {
+router.get('/users/me', async (req: Request, res: Response) => {
+    const token = validationRequest(req);
     try {
-        res.send(await userService.getUser("it-drixit-1"));
+        res.send(await userService.getUser({ jwt: token }));
     } catch (error: any) {
         res.status(404).send({ error: error.message });
     }
 });
 
-router.get('/users', (_req, res) => {
-    res.send(userService.getUsers());
+router.get('/users', async (req: Request, res: Response) => {
+    const token = validationRequest(req);
+    try {
+        res.send(await userService.getUsers({ jwt: token }));
+    } catch (error: any) {
+        res.status(404).send({ error: error.message });
+    }
 });
 
-// router.post('/users/new', async (_req, res) => {
-//     try {
-//         res.send(await userService.createUser());
-//     } catch (error: any) {
-//         res.status(500).send({ error: error.message });
-//     }
-// });
+router.post('/users/new', async (req: Request, res: Response) => {
+    try {
+        res.send(await userService.createUser(req.body));
+    } catch (error: any) {
+        res.status(500).send({ error: error.message });
+    }
+});
 
 export default router;
 
