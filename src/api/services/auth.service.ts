9c60ee8a9541b3e9bc8288ab89a9d6160d856aca
Wolffoner <wolffoner@gmail.com>
2022-08-20 19:45:26 -0300

    services

---
+++ src/api/services/auth.service.ts
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import { ServerUser } from "../models/interfaces.ts/user.interface";
+import userService from "./user.service";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Token } from "../models/interfaces.ts/token.interface";
+class AuthService {
+
+    constructor() {
+    }
+
+    createToken(email: string): Token {
+        const token = jwt.sign({ email }, process.env.JWT_SECRET || "secret", { expiresIn: '1h' });
+        return { jwt: token };
+    }
+
+    verifyToken(token: string): JwtPayload {
+        const decodedToken = jwt.verify(token, 'secretkey');
+        return decodedToken as JwtPayload;
+    }
+    async setToken(user: ServerUser): Promise<Token> {
+        try {
+            if (!user?.email || !user?.password) {
+                throw new Error("Email and password are required");
+            }
+            const serverUser: ServerUser = await userService.getUserWithPassword(user.email);
+            if (serverUser.password !== user.password) {
+                throw new Error("Login unsuccessful");
+            }
+            return this.createToken(serverUser.email);
+        } catch (error: any) {
+            throw new Error(error.message);
+        }
+    }
+
+}
+
+export default new AuthService();
--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,35 +1,48 @@
-import { User } from '../models/schemas/user.schemas';
-import { ClientUser, ServerUser } from '../models/user.model';
+import { User } from '../models/schemas/user.schema';
+import { ClientUser, ServerUser } from '../models/interfaces.ts/user.interface';
+import authService from './auth.service';
+import { Token } from '../models/interfaces.ts/token.interface';
 class UserService {
 
     constructor() {
     }
 
-    async getUsers(): Promise<ClientUser[]> {
+    async getUsers(token: Token): Promise<ClientUser[]> {
         try {
-            const user = await User.findOne({}, { _id: 0, __v: 0, password: 0 });
-            return user?.toJSON() as ClientUser[];
+            authService.verifyToken(token.jwt);
+            const users = await User.find({}, { _id: 0, __v: 0, password: 0 });
+            return users as ClientUser[];
         } catch (error) {
             throw new Error("Error user not found");
         }
     }
 
-    async getUser(id: string): Promise<ClientUser> {
+    async getUser(token: Token): Promise<ClientUser> {
         try {
-            const user = await User.findOne({ id }, { _id: 0, __v: 0, password: 0 });
+            const { email } = authService.verifyToken(token.jwt);
+            const user = await User.findOne({ email }, { _id: 0, __v: 0, password: 0 });
             return user?.toJSON() as ClientUser;
         } catch (error) {
             throw new Error("Error user not found");
         }
     }
 
-    async createUser(serverUser: ServerUser): Promise<ClientUser> {
+    async getUserWithPassword(email: string): Promise<ServerUser> {
+        try {
+            const user = await User.findOne({ email }, { _id: 0, __v: 0 });
+            return user?.toJSON() as ServerUser;
+        } catch (error) {
+            throw new Error("Error user not found");
+        }
+    }
+
+    async createUser(serverUser: ServerUser): Promise<Token> {
         const user = new User({
             ...serverUser
         })
         try {
             await user.save();
-            return user.toJSON() as ClientUser;
+            return authService.createToken(serverUser.email);
         } catch (error) {
             throw new Error("Error user creation");
         }
