ec3ee8ddefe89d817a15720fa4f6d232227f1ccf
Wolffoner <wolffoner@gmail.com>
2022-08-20 19:45:54 -0300

    changes on main

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import express from 'express';
+import 'dotenv/config';
 import cors from 'cors';
 import userRoutes from './api/routes/user.routes';
 import authRoutes from './api/routes/auth.routes';
 import database from './api/config/database.config';
-
 
 const main = async () => {
     const app = express();
     app.use(express.json());
     app.use(cors());
 
-    //TODO: .env file
-    const PORT = 3000;
-
-    database.connect();
+    database.connect(process.env.DATABASE || '');
+    const PORT = process.env.PORT || 3000;
 
     app.use('/api/v0', userRoutes);
     app.use('/api/v0', authRoutes);
